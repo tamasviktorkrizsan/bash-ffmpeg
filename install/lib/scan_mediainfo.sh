0b4269cd094b2492a395d2ac0ddc71b92e25ad82
Tamás Krizsán <tamas@krizsanmunka.hu>
2021-04-13 22:09:04 +0200

    feat: add mediainfo based media scan function

---
+++ install/lib/scan_mediainfo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# part of bash-ffmpeg-functions Copyright (C) 2020 Tamas Viktor Krizsan
+# <https://github.com/tamasviktorkrizsan>
+# License: GPL-3.0-or-later
+
+
+
+### INCLUDES
+
+source bash.inc.sh
+
+source mediainfo.sh
+
+
+### FUNCTIONS
+
+scan_mediainfo () {
+
+## Input Parameters
+
+usr_input=${1:-*.webm *.mp4 *.mkv *.mov *.wmv *.mpeg *.mpg *.flv *.avi *.divx *.vob *.264 *.h264 *.wav *.flac *.m4a *.mp3};
+
+
+### Settings
+
+media_log_suffix="_scan_mediainfo.log";
+
+
+### Executing Command
+
+for i in $usr_input;
+do mediainfo "$i" --Output=JSON 2>&1 |\
+tee "${i%}_$media_log_suffix";
+done;
+
+}
